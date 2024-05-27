3dcc0754d6edcb7c7d2ab3ab109044dd5e8f9afc
Владислав <megoboom17@gmail.com>
2024-05-27 19:26:19 +0300

    nullpointerexception fix

--- app/src/main/java/hexlet/code/DiffBuilder.java
+++ app/src/main/java/hexlet/code/DiffBuilder.java
@@ -1,6 +1,5 @@
 package hexlet.code;
 
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,7 +10,7 @@ public class DiffBuilder {
 
     private List<Map<String, String>> diff = new ArrayList<>();
 
-    public List<Map<String, String>>  build(Map<String, Object> map1, Map<String, Object> map2) {
+    public List<Map<String, String>> build(Map<String, Object> map1, Map<String, Object> map2) {
 
         var keySet1 = map1.keySet().stream().toList();
         var keySet2 = map2.keySet().stream().toList();
@@ -23,25 +22,30 @@ public class DiffBuilder {
 
         for (String k : combinedKeyList) {
             Map<String, String> node = new HashMap<>();
-            if (keySet1.contains(k) && keySet2.contains(k)) {
-                if (map1.get(k).equals(map2.get(k))) {
+            boolean containsKey1 = keySet1.contains(k);
+            boolean containsKey2 = keySet2.contains(k);
+            Object value1 = map1.get(k);
+            Object value2 = map2.get(k);
+
+            if (containsKey1 && containsKey2) {
+                if (value1 != null && value1.equals(value2)) {
                     node.put("key", k);
                     node.put("state", "notchanged");
-                    node.put("actualValue", map1.get(k).toString());
+                    node.put("actualValue", value1.toString());
                 } else {
                     node.put("key", k);
                     node.put("state", "changed");
-                    node.put("prevValue", map1.get(k).toString());
-                    node.put("actualValue", map2.get(k).toString());
+                    node.put("prevValue", value1 != null ? value1.toString() : "null");
+                    node.put("actualValue", value2 != null ? value2.toString() : "null");
                 }
-            } else if (keySet1.contains(k)) {
+            } else if (containsKey1) {
                 node.put("key", k);
                 node.put("state", "deleted");
-                node.put("prevValue", map1.get(k).toString());
+                node.put("prevValue", value1 != null ? value1.toString() : "null");
             } else {
                 node.put("key", k);
                 node.put("state", "new");
-                node.put("actualValue", map2.get(k).toString());
+                node.put("actualValue", value2 != null ? value2.toString() : "null");
             }
             diff.add(node);
         }
