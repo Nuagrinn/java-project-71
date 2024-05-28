f8ff4f5a84432ad92d0b77bc99d9ffea0f6f7217
Владислав <megoboom17@gmail.com>
2024-05-28 20:44:56 +0300

    SuppressWarnings("MagicNumber")

--- app/src/test/java/TestParser.java
+++ app/src/test/java/TestParser.java
@@ -8,7 +8,7 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@SuppressWarnings("checkstyle:MagicNumber")
+@SuppressWarnings("MagicNumber")
 public class TestParser {
 
     private static Map<String, Object> expectedParsed = new HashMap<>();
